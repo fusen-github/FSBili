d8a8dd50b9205837f628c883c4a5c706f9716f66
fusen <fusen@163.com>
2016-03-09 23:09:12 +0800

    初始化FSBili项目

---
+++ FSBliBli/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSBliBli
+//
+//  Created by 四维图新 on 16/3/9.
+//  Copyright © 2016年 四维图新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FSBliBli/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FSBliBli
+//
+//  Created by 四维图新 on 16/3/9.
+//  Copyright © 2016年 四维图新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
